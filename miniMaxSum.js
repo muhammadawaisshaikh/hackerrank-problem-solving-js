b915e69b4df884fa9103f9127126e7525c52280f
Muhammad Awais <awaisshaikh94@gmail.com>
2019-11-27 17:28:57 +0500

    13 test case passed. success all

--- miniMaxSum.js
+++ miniMaxSum.js
@@ -4,15 +4,27 @@ function miniMaxSum(arr) {
     let sorted = [];
     let sum = 0;
 
-    for(let i=0; i<arr.length; i++) {
-        sum = 0;
-       for(let j=0; j<arr.length; j++) {
-           if(arr[i] != arr[j]) {
-               sum = sum+arr[j];
-           }
+    const allEqual = arr => arr.every( v => v === arr[0] )
+
+    if(allEqual(arr) == true) {
+        for(let k=0; k<arr.length-1; k++) {
+            sum = sum+arr[k];
         }
-        arr1.push(sum); 
+        arr1.push(sum);
+        arr1.push(sum);
     }
+    else {
+        for(let i=0; i<arr.length; i++) {
+            sum = 0;
+           for(let j=0; j<arr.length; j++) {
+               if(arr[i] != arr[j]) {
+                   sum = sum+arr[j];
+               }
+            }
+            arr1.push(sum); 
+        }
+    }
+
     sorted = arr1.sort();
     console.log(sorted[0]+' '+sorted[arr1.length-1]);
 }
